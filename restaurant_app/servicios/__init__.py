7121e0b34cba9ba4e0314bfb24ffc0cd453d9063
RAlcoser <ar.alcoser@gmail.com>
2026-08-28 20:30:26 -0500

    README.md Corregido y  añado comentarios

--- restaurant_app/servicios/__init__.py
+++ restaurant_app/servicios/__init__.py
@@ -1,6 +1,7 @@
-# Archivo requerido para identificar el paquete servicios. Crear los archivos __init__.py correspondientes
-# en los paquetes es parte de la buena practica de la POO. Este archivo identifica a servicios/ como paquete
-# de Python, permitiendo las importaciones como "from servicios.restaurante import Restaurante".
+# Archivo requerido para identificar el paquete servicios. Crear los archivos __init__.py correspondientes en los paquetes es parte de 
+# la buena practica de la POO. Este archivo identifica a servicios/ como paquete de Python, permitiendo las importaciones como 
+# "from servicios.restaurante import Restaurante".
+
 from .restaurante import Restaurante
 from .archivo_servicio import ArchivoServicio
 
